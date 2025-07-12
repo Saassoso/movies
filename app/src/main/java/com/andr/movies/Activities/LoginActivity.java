c66b57e1c4d2310b612cf13412960fe1bc7679b1
Saassoso <charifsaad38@gmail.com>
2025-07-12 03:22:05 +0100

    add login, register and search layoyuts

---
+++ app/src/main/java/com/andr/movies/Activities/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.andr.movies.Activities;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
---
+++ app/src/main/java/com/andr/movies/Activities/RegisterActivity.java
@@ -0,0 +1,6 @@
+package com.andr.movies.Activities;
+
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+}
